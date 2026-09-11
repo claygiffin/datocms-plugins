619688e6e1836905cc8861625b7c7b70e5088e93
Clay Giffin <clay@claygiffin.com>
2026-09-11 08:21:04 -0700

    out of sync matching

--- src/plugins/sync-blocks-button/SyncBlocksButton.tsx
+++ src/plugins/sync-blocks-button/SyncBlocksButton.tsx
@@ -601,7 +601,7 @@ export const SyncBlocksButton: React.FC<Props> = ({ ctx }) => {
     }
   }
 
-  const currentValue = get(ctx.formValues, ctx.fieldPath) !== 'OUT OF SYNC'
+  const currentValue = get(ctx.formValues, ctx.fieldPath) === ' '
   const isSynced = Boolean(isValid === null ? currentValue : isValid)
   const isDisabled = loading || syncing || isSynced
 
